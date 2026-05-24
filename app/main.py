0b78fe54e683dc31360776329c56f007a43e2ac5
Leo Segal <ldsegal@mtu.edu>
2026-05-24 14:28:12 -0400

    Added login screen

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@
 from gevent import monkey
 monkey.patch_all()  # Patch standard library for gevent compatibility (for WebSockets)
 
+import os
 import atexit
 import threading
+from dotenv import load_dotenv
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 from app.camera import stream
@@ -16,6 +18,7 @@ stream_idle_timer = None
 stream_idle_timeout = 10.0 # Seconds of inactivity before pausing stream
 
 # Web app setup
+load_dotenv()
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins="*") # TODO test without this security setting
 
@@ -24,6 +27,31 @@ socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins="*") # TODO t
 def index():
     """Serves the main web control interface"""
     return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    """New client connected (to login screen)"""
+    print('Client Connected. Awaiting password...')
+
+@ app.route('/check_password', methods=['POST'])
+def check_password():
+    """Simple password check for login"""
+    pw = request.json.get('password')
+    if pw == os.getenv('PASSWORD'):
+        add_client()
+        return {'success': True}, 200
+    return {'success': False}, 401
+
+def add_client():
+    """Add client to stream"""
+    global num_clients, stream_idle_timer
+    num_clients += 1
+    print(f'Client joined. Active Viewers: {num_clients}')
+    if stream_idle_timer:
+        stream_idle_timer.cancel() # Cancel any existing timer
+        stream_idle_timer = None
+    if stream.is_idle():
+        stream.start() # Start stream if it was idle
 
 @app.route('/set_laser', methods=['POST'])
 def set_laser():
@@ -38,17 +66,13 @@ def set_crosshair():
     imageProcessor.enable_crosshair(request.json['value'])
     return '', 204
 
-@socketio.on('connect')
-def handle_connect():
-    """New client connected"""
-    global num_clients, stream_idle_timer
-    num_clients += 1
-    print(f'Client connected. Active Viewers: {num_clients}')
-    if stream_idle_timer:
-        stream_idle_timer.cancel() # Cancel any existing timer
-        stream_idle_timer = None
-    if stream.is_idle():
-        stream.start() # Start stream if it was idle
+@socketio.on('joystick_move')
+def handle_joystick_move(data):
+    """
+    Receives continuous coordinate streams from the frontend joystick
+    """
+    x = data.get('x', 0)
+    y = data.get('y', 0)
 
 @socketio.on('disconnect')
 def handle_disconnect():
@@ -62,14 +86,6 @@ def handle_disconnect():
         stream_idle_timer = threading.Timer(stream_idle_timeout, stream.idle)
         stream_idle_timer.start() # Set timer to idle stream
 
-@socketio.on('joystick_move')
-def handle_joystick_move(data):
-    """
-    Receives continuous coordinate streams from the frontend joystick
-    """
-    x = data.get('x', 0)
-    y = data.get('y', 0)
-
 @atexit.register
 def global_shutdown():
     """Shutdown handler to clean up resources on exit"""
@@ -78,6 +94,6 @@ def global_shutdown():
 # Development testing entry point
 if __name__ == '__main__':
     try:
-        socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+        socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=False)
     except KeyboardInterrupt:
         pass
---
+++ app/static/script.js
@@ -0,0 +1,84 @@
+
+// Elements
+const passwordInput = document.getElementById("input")
+const loginContainer = document.getElementById("login")
+const appContainer = document.getElementById("app-container")
+const video = document.getElementById("stream");
+const status = document.getElementById("status");
+
+// SocketIO Connection
+var socket = io({
+    path: '/socket.io/'
+});
+
+// Login Listener
+passwordInput.addEventListener("keypress", async function (event) {
+    if (event.key === "Enter") {
+        const response = await fetch('/check_password', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ password: passwordInput.value })
+        });
+        const result = await response.json();
+        if (result.success) {
+            loginContainer.style.display = "none";
+            appContainer.style.display = "block";
+            connect();
+        } else {
+            passwordInput.value = "";
+            passwordInput.style.borderColor = "red";
+            setTimeout(() => {
+                passwordInput.style.borderColor = "";
+            }, 500);
+        }
+    }
+});
+
+// WebRTC Video Connection
+async function connect() {
+    const pc = new RTCPeerConnection();
+
+    pc.ontrack = (e) => {
+        status.style.display = "none";
+        video.style.display = "block";
+        video.srcObject = e.streams[0];
+    };
+    pc.addTransceiver("video", { direction: "recvonly" });
+
+    try {
+        const offer = await pc.createOffer();
+        await pc.setLocalDescription(offer);
+
+        const res = await fetch("/stream/laserCam/whep", {
+            method: "POST",
+            headers: { "Content-Type": "application/sdp" },
+            body: pc.localDescription.sdp
+        });
+
+        if (res.status !== 200 && res.status !== 201) {
+            throw new Error("Stream not ready");
+        }
+
+        const answer = await res.text();
+        await pc.setRemoteDescription({ type: "answer", sdp: answer });
+
+    } catch (err) {
+        setTimeout(connect, 500);
+    }
+}
+
+// Set fullscreen on rotate
+window.addEventListener("orientationchange", () => {
+    if (window.orientation === 90 || window.orientation === -90) {
+        const app = document.getElementById("app-container");
+        if (app && !document.fullscreenElement) {
+            app.requestFullscreen().catch(err => {
+                console.log("Fullscreen request failed: " + err.message);
+            });
+        }
+    } else {
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+        }
+    }
+});
